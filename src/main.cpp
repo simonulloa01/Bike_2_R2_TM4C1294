5f26372a5c9ccce986b1f6357ec709f6706da654
Simon <simon_ulloa@outlook.com>
2025-03-22 16:30:25 -0400

    init config

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <libopencm3/lm4f/gpio.h>
+#include <libopencm3/lm4f/systemcontrol.h>
+
+int main(void) {
+
+    periph_clock_enable(RCC_GPION);
+    
+    // Configure PN0 as output
+    gpio_mode_setup(GPION_BASE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO0);
+    
+    while (1) {
+        // Toggle PN0
+        gpio_toggle(GPION_BASE, GPIO0);
+        
+        // Delay
+        for (int i = 0; i < 1000000; i++) {
+            __asm__("nop");
+        }
+    }
+    
+    return 0;
+}
