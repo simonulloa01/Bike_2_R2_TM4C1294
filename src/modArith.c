af6849b5a99ff68b281bac62b10c8a8d620b7dd8
Ashley Diaz <ash305miami@gmail.com>
2025-03-28 17:09:27 -0400

    Added modAdd function

--- src/modArith.c
+++ src/modArith.c
@@ -7,10 +7,18 @@ void modInv(uint8_t *dst, const uint8_t *src)
 
 void modMult(uint8_t *dst, const uint8_t *a, const uint8_t *b)
 {
-    // todo
+    //
 }
 
 void modAdd(uint8_t *dst, const uint8_t *a, const uint8_t *b)
 {
-    //todo
+    // Do not perform arithmetic on polynomial's a and b if they are NULL. 
+    if ((a != NULL) && (b != NULL))
+    {
+        // Adding the two binary polynomials is the equivalent of the XOR
+        // operation. One of the reasons for this is because the inverse of
+        // number is itself. For example, 1 plus 1 is 0, since the number 
+        // is its own inverse.  
+        *dst = *a ^ *b;
+    }
 }
--- src/modArith.h
+++ src/modArith.h
@@ -3,6 +3,11 @@
 
 #include "defs.h"
 
+// Define the irreducible polynomial for modular operations. 
+// this will be a polynomial depicted as a binary number with a 
+// 1 in the LSB bit and the MSB or bit 10163 (R_BITS).
+#define IRREDUCIBLE_POLY 
+
 void modInv(uint8_t *dst, const uint8_t *src);
 
 void modMult(uint8_t *dst, const uint8_t *a, const uint8_t *b);
