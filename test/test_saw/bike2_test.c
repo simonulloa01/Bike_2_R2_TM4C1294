11b0c914eec11595e2946ce4d2a2b7418fcddd7a
Ashley Diaz <ash305miami@gmail.com>
2025-04-01 22:11:06 -0400

    added additional info to saw test files.

--- test/test_saw/bike2_test.c
+++ test/test_saw/bike2_test.c
@@ -1,16 +1,22 @@
-#include <stdio.h>
+#include <stdint.h>
+
+// Constants
+#define BITS_IN_BYTE 8
 
 // Functions to test
 int addNums(int a, int b);
 
-int main ()
-{
-    int sum = addNums(4, 5);
-    printf("Sum: %d\n", sum);
-    return 0;
-}
+// int main ()
+// {
+//     int sum = addNums(4, 5);
+//     printf("Sum: %d\n", sum);
+//     return 0;
+// }
 
+// Test function
 int addNums(int a, int b)
 {
     return a + b;
 }
+
+// Bike Specific Functions
